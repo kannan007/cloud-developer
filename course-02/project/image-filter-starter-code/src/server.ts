d9aa284cfbeafd5670ee83744682dc8c25c051b8
Kannan <kannant92@yahoo.in>
2021-08-04 11:51:42 +0530

    status code changed

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -41,7 +41,7 @@ const isImageUrl = require('is-image-url');
   app.get("/filteredimage", async(req,res)=> {
     const {image_url} = req.query
 
-    if(!image_url || !isImageUrl(image_url)) return res.status(417).send('Malformed URL!..Try again with a proper image URL') 
+    if(!image_url || !isImageUrl(image_url)) return res.status(400).send('Malformed URL!..Try again with a proper image URL') 
 
     const filteredpath: string = await filterImageFromURL(image_url)
     res.status(200).sendFile(filteredpath,()=> {
